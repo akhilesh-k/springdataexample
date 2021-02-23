9d7e5196658b88725420975df5c8c3bf0f7afe35
Akhilesh Kumar <akhilesh-k@outlook.com>
2021-02-23 20:39:17 +0530

    Implemented Redis connectivity

--- src/main/java/com/example/hibernateexample/entity/Employee.java
+++ src/main/java/com/example/hibernateexample/entity/Employee.java
@@ -2,14 +2,13 @@ package com.example.hibernateexample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.redis.core.RedisHash;
 
-@Document
+@RedisHash
 @Getter
 @Setter
 public class Employee {
-	@MongoId
+
 	private long id;
 	private String name;
 	private String departmentName;
